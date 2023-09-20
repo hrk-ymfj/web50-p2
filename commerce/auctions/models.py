1f8d7d9e65a7b771febe35463937be6fcf202f7a
H.Yamafuji <stlas2000@gmail.com>
2023-09-20 18:09:08 +0900

    マイグレーション完了

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -20,31 +20,28 @@ class AuctionListing(models.Model):
     current_bid = models.DecimalField(max_digits=10, decimal_places=2)
     image_url = models.URLField(blank=True, null=True)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
-    seller = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    seller = models.ForeignKey(User, on_delete=models.CASCADE, related_name='listings')  # sellerの逆参照名を'listings'に設定
     closed = models.BooleanField(default=False)
-    winner = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True, blank=True)
+    winner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='won_listings')  # winnerの逆参照名を'won_listings'に設定
 
     def __str__(self):
         return self.title
+
 
 # 入札モデル
 class Bid(models.Model):
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     listing = models.ForeignKey(AuctionListing, on_delete=models.CASCADE)
-    bidder = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    bidder = models.ForeignKey(User, on_delete=models.CASCADE)
 
 # ウォッチリストモデル
 class Watchlist(models.Model):
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     listings = models.ManyToManyField(AuctionListing)
 
 # コメントモデル
 class Comment(models.Model):
     text = models.TextField()
     listing = models.ForeignKey(AuctionListing, on_delete=models.CASCADE)
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-
-# Django管理インターフェースに表示するカスタムモデル
-class AdminView(models.Model):
-    # 管理者用のビューをここに追加する (例: リスト、コメント、入札など)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
