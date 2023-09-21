f5da286412560102d93b8872a604052838424608
H.Yamafuji <stlas2000@gmail.com>
2023-09-21 09:27:21 +0900

    モデルと管理画面の作成が完了

--- commerce/auctions/admin.py
+++ commerce/auctions/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
+from django.contrib.auth.models import User
 
 # Register your models here.
+# models.pyに存在する全モデルを、管理者サイトで変更可能にする
+admin.site.register(User)
+admin.site.register(Category)
+admin.site.register(AuctionListing)
+admin.site.register(Bid)
+admin.site.register(Comment)
+admin.site.register(Watchlist)
+
